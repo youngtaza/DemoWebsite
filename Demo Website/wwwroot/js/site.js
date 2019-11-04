b93a3b70f9adc2c4a547861dc09b5ed2fc1437b6
youngtaza <therealmurtaza@gmail.com>
2019-11-04 14:27:05 -0600

    Worked on navbar to add expand/collpase

--- Demo Website/wwwroot/js/site.js
+++ Demo Website/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+/* To add and remove Expanded class to navbar */
+function AddExpandedClass() {
+    document.getElementById("navitem_2").className = "navbar-item navbar-expanded";
+    document.getElementById("navitem_expcolbtn1").className = "fas fa-angle-up";
+    document.getElementById("navitem_expcolbtn1").setAttribute("onclick", "RemoveExpandedClass;");
+}
+
+function RemoveExpandedClass() {
+    document.getElementById("navitem_2").className = "navbar-item navbar-collapsed";
+    document.getElementById("navitem_expcolbtn1").className = "fas fa-angle-down";
+    document.getElementById("navitem_expcolbtn1").setAttribute("onclick", "AddExpandedClass;");
+}
+
