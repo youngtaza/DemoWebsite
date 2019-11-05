cd1dbadb59c6335442ddc6e186ffe496a834c72b
youngtaza <therealmurtaza@gmail.com>
2019-11-04 20:15:28 -0600

    Added JQuery function to append "Mobile" class to HTML page based on screensize. Same function also removes hover functions on Mobile vs Desktop.

--- Demo Website/wwwroot/js/site.js
+++ Demo Website/wwwroot/js/site.js
@@ -6,13 +6,36 @@
 /* To add and remove Expanded class to navbar */
 function AddExpandedClass() {
     document.getElementById("navitem_2").className = "navbar-item navbar-expanded";
-    document.getElementById("navitem_expcolbtnicon1").className = "fas fa-angle-up";
+    document.getElementById("navitem_expcolbtnicon1").className = "fas fa-angle-up navbar-faicon";
     document.getElementById("navitem_expcolbtn1").setAttribute("onclick", "RemoveExpandedClass()");
 }
 
 function RemoveExpandedClass() {
     document.getElementById("navitem_2").className = "navbar-item navbar-collapsed";
-    document.getElementById("navitem_expcolbtnicon1").className = "fas fa-angle-down";
+    document.getElementById("navitem_expcolbtnicon1").className = "fas fa-angle-down navbar-faicon";
     document.getElementById("navitem_expcolbtn1").setAttribute("onclick", "AddExpandedClass()");
 }
 
+
+(function ($) {
+    var $window = $(window),
+        $html = $('html'),
+        $navitem = $('#navitem_2');
+        
+
+    function resize() {
+        if ($window.width() < 514) {
+            $navitem.removeAttr('onmouseover');
+            $navitem.removeAttr('onmouseout');
+            return $html.addClass('mobile');
+        }
+
+        $html.removeClass('mobile');
+        $navitem.attr('onmouseover', 'AddExpandedClass();');
+        $navitem.attr('onmouseout', 'RemoveExpandedClass();');
+    }
+
+    $window
+        .resize(resize)
+        .trigger('resize');
+})(jQuery);
